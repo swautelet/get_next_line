71e08ca3ad3d1045bc83643c5453c37ecd603aab
Simon Wautelet <simonwautelet@MacBook-Pro-de-Simon.local>
2021-11-11 21:30:22 +0100

    autosave

---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: simonwautelet <simonwautelet@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/11 22:11:36 by simonwautel       #+#    #+#             */
+/*   Updated: 2021/11/11 21:27:07 by simonwautel      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include"get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	l;
+
+	l = -1;
+	while (str[++l])
+	{
+		if (str[l] == '\n')
+		{
+			l++;
+			break ;
+		}
+	}
+	l += 2;
+	return (l);
+}
+
+char	*ft_straddback(char *result, char *buffer, ssize_t size)
+{
+	char	*newresult;
+	ssize_t	i;
+	ssize_t	l;
+
+	i = -1;
+	newresult = malloc(sizeof(char) * (size + ft_strlen(result)));
+	if (!newresult)
+	{
+		free (result);
+		return (NULL);
+	}
+	while (result[++i])
+		newresult[i] = result[i];
+	free (result);
+	l = -1;
+	while (buffer[++l] && size - l > 0)
+	{
+		newresult[i + l] = buffer[l];
+		if (buffer[l] == '\n')
+		{
+			buffer[l++] = -1;
+			break ;
+		}
+		buffer[l] = -1;
+	}
+	newresult[i + l] = '\0';
+	if (size == l)
+		while (buffer[l])
+			buffer[l++] = -1;
+	return (newresult);
+}
+
+int	ft_end_of_line(char *str)
+{
+	while (*str)
+	{
+		if (*str == '\n')
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*ft_initialize(char *buffer)
+{
+	char		*new;
+	ssize_t		i;
+	ssize_t		l;
+
+	i = 0;
+	while (buffer[i] && buffer[i] == -1)
+		i++;
+	new = malloc((ft_strlen(&buffer[i])) * sizeof(char));
+	if (!new)
+		return (NULL);
+	if (!buffer[i])
+	{
+		new[0] = '\0';
+		return (new);
+	}
+	l = 0;
+	while (buffer[i + l] && buffer[i + l] != -1)
+	{
+		new[l] = buffer[i + l];
+		buffer[i + l] = -1;
+		l++;
+		if (new[l - 1] == '\n')
+			break ;
+	}
+	new[l] = '\0';
+	return (new);
+}
