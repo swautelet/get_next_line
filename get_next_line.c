6a735744fa7e8edb6c87a4ef2ecb19492e24e405
Simon Wautelet <simonwautelet@MacBook-Pro-de-Simon.local>
2021-11-13 13:05:55 +0100

    autosave

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: simonwautelet <simonwautelet@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 22:01:05 by simonwautel       #+#    #+#             */
-/*   Updated: 2021/11/11 22:01:06 by simonwautel      ###   ########.fr       */
+/*   Updated: 2021/11/13 13:03:17 by simonwautel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,18 +26,18 @@ char	*gnl(char *result, char *buffer, int size, int fd)
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[FOPEN_MAX][BUFFER_SIZE + 1];
+	static char	buffer[BUFFER_SIZE + 1];
 	char		*result;
 	ssize_t		size;
 
 	if (fd < 0 || fd > FOPEN_MAX)
 		return (NULL);
-	result = ft_initialize(buffer[fd]);
+	result = ft_initialize(buffer);
 	if (!result)
 		return (NULL);
 	if (ft_end_of_line(result) == 1)
 		return (result);
-	size = read(fd, buffer[fd], BUFFER_SIZE);
+	size = read(fd, buffer, BUFFER_SIZE);
 	if (size == 0 && result[0] != '\0')
 		return (result);
 	if (size <= 0)
@@ -45,6 +45,6 @@ char	*get_next_line(int fd)
 		free (result);
 		return (NULL);
 	}
-	result = gnl(result, buffer[fd], size, fd);
+	result = gnl(result, buffer, size, fd);
 	return (result);
 }
